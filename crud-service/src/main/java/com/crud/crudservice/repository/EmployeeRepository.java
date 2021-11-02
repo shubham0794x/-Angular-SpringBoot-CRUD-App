8e381f07cb212e5b0c91b9eb13f0dbc8ae83f560
Shubham Bhadani <shubham.bhadani07@gmail.com>
2021-11-02 20:03:06 +0530

    Rest Service

---
+++ crud-service/src/main/java/com/crud/crudservice/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.crud.crudservice.repository;
+
+import com.crud.crudservice.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
